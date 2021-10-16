a98221cc5a57d1331b15ed6549288e809bc22f1e
sami1310 <samia.jahin@northsouth.edu>
2021-10-16 20:23:13 +0600

    changes in server.js

--- server.js
+++ server.js
@@ -6,11 +6,22 @@ if (process.env.NODE_ENV !== 'production'){
 const RAPID_API_KEY = process.env.RAPID_API_KEY  //for calling API key inside from the server rather then exposing it in the client side
 const express = require('express')
 const app = express()
+const axios = require('axios')
 
 app.use(express.json())
 app.use(express.static('public'))
 
-app.post('/weather', (req, res) => {
+app.post('/Luna', (req, res) => {
+       console.log(req.body)
+    /*const options = {
+        method: 'GET',
+        url: 'https://weatherapi-com.p.rapidapi.com/ip.json',
+        params: {q: 'searchBox'},
+        headers: {
+          'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com',
+          'x-rapidapi-key': RAPID_API_KEY
+        }
+      }*/
 
 })
 
