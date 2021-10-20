1d78a5eaa3d13b8f3c4f2f7dc5d86355ff809cc9
sami1310 <samia.jahin@northsouth.edu>
2021-10-20 19:20:39 +0600

    works fine with both google maps api and rapid api

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
 
 //let map;
 
+//const { response } = require("express");
+
 function initMap() {
 
-const searchElement = document.querySelector('[data-city-search]')
-const searchBox = new google.maps.places.SearchBox(searchElement)
-
-const geocoder = new google.maps.Geocoder();
-const infowindow = new google.maps.InfoWindow();
-
-
-//searchBox.addEventListener("click", () => {
-searchBox.addListener('places_changed', () => {   //listens for the searchBox
-  const place = searchBox.getPlaces()[0]
-  if (place == null) return
-  const latLong = new google.maps.LatLng(place)
-  //console.log(latLong)
-  fetch('/Luna',{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept' : 'application/json'
-        },
-        body : JSON.stringify({
-            latitude: place.geometry.location.lat(),//latLong.lat(),
-            longitude: place.geometry.location.lng()//latLong.lng()
-        })
-
-      }).then(res => res.json()).then(data => {
-          setMoonCastData(data, place.formatted_address)
-      })
-
-//}
-    })}
+    const searchElement = document.querySelector('[data-city-search]')
+    const searchBox = new google.maps.places.SearchBox(searchElement)
+    
+    const geocoder = new google.maps.Geocoder();
+    const infowindow = new google.maps.InfoWindow();
+    
+    
+    //searchBox.addEventListener("click", () => {
+    searchBox.addListener('places_changed', () => {   //listens for the searchBox
+      const place = searchBox.getPlaces()[0]
+      if (place == null) return
+      const latLong = new google.maps.LatLng(place)
+      //console.log(latLong)
+      fetch('/Luna',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept' : 'application/json'
+            },
+            body : JSON.stringify({
+                latitude: place.geometry.location.lat(),//latLong.lat(),
+                longitude: place.geometry.location.lng()//latLong.lng()
+            })
+    
+          }).then(res => res.json()).then(data => {
+              //setMoonCastData(data, place.formatted_address)
+              console.log(data)
+          })
+    
+    //}
+        })}
+    
 
   /*fetch('/mooncast',{
     method: 'POST',
